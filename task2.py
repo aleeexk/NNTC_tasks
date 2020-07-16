4ae864c690bff0381e36cb366cc0a1ac955b2c9b
aleeexk <53943917+aleeexk@users.noreply.github.com>
2020-07-16 14:06:07 +0700

    Add files via upload

--- task2.py
+++ task2.py
@@ -18,8 +18,6 @@ with open('csv.csv') as f:
 
 dict1 = dict(zip(headers, initial))
 dict2 = dict(zip(headers, ended))
-print(dict1.keys())
-print(dict1.values())
 
 with open('bc_dicts.txt', 'w') as out:
     out.write('Begin')
@@ -28,3 +26,33 @@ with open('bc_dicts.txt', 'w') as out:
     out.write('\nEnd')
     for key, val in dict2.items():
         out.write('{}:{}\n'.format(key, val))
+
+i = 7
+bigdict = []
+while i < 57:
+    dictn = dict(zip(headers, alot[i]))
+    bigdict.append(dictn)
+    i = i + 1
+
+n = 0
+while n < 50:
+    x = (bigdict[n].get('pressure'))
+    x = float(x) / 100000
+    bigdict[n]['pressure'] = x
+    n = n + 1
+
+m = 0
+while m < 50:
+    x = (bigdict[m].get('temperature'))
+    x = float(x) - 273
+    bigdict[m]['temperature'] = x
+    m = m + 1
+
+with open('new.tsv', 'wt') as out_file:
+    tsv_writer = csv.writer(out_file, delimiter='\t')
+    headers.pop(0)
+    tsv_writer.writerow(headers)
+    m = 0
+    while m < 50:
+        tsv_writer.writerow(bigdict[m].values())
+        m = m + 1
